23ba9628706583dac84fc96011b92e1b02fccd0d
chefmohima <emailmohimac@gmail.com>
2020-06-21 02:08:57 +0530

    modified templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
